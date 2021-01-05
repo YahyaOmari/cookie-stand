e2aeac5ee0df0a35395d7c325a8a5abb4a333b45
YahyaOmari <yahya.omari@outlook.com>
2021-01-05 15:17:55 +0200

    still needwork

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var workHours = ["6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM"];
+var tableHead = ["city", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "Daily Location Total"];
 var locationTable = [];
 var parentElement = document.getElementById("SalmonCookies");
 var parentElementTable = document.getElementById("TotalSold");
@@ -15,11 +16,11 @@ function Places(location, min, max, avgCookie) {
     this.cookieForHour = [];
 }
 
-var seattlLocation = new Places('Seattl', 23, 65, 6.3);
-var tokyoLocation = new Places('Tokyo', 3, 24, 1.2);
-var dubaiLocation = new Places('Dubai', 11, 38, 3.7);
-var parisLocation = new Places('Paris', 20, 38, 2.3);
-var limaLocation = new Places('Lima', 2, 16, 4.6);
+var seattlLocation = new Places('Seattl: ', 23, 65, 6.3);
+var tokyoLocation = new Places('Tokyo: ', 3, 24, 1.2);
+var dubaiLocation = new Places('Dubai: ', 11, 38, 3.7);
+var parisLocation = new Places('Paris: ', 20, 38, 2.3);
+var limaLocation = new Places('Lima: ', 2, 16, 4.6);
 
 // creating random functionMath function through constructors
 Places.prototype.generateRandomNum = function () {
@@ -60,21 +61,24 @@ Places.prototype.display = function () {
 
 // console.log(seattlLocation.display());
 
-
-//var workHours = ["6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "Daily Location Total"];
-// aray inside it the variables and data 
+var parentElementTable = document.getElementById("TotalSold");
 
 Places.prototype.renderTable = function () {
     // creating a new element
-    
-    var article = document.createElement('article');
-    parentElementTable.appendChild(article);
 
+    // var parentElementTable = document.getElementById("TotalSold");
+
+    var table = document.createElement('table');
+    parentElementTable.appendChild(table);
+    
     var tr = document.createElement('tr');
+    table.appendChild(tr);
+
     var th = document.createElement('th');
+    // article.appendChild(th);
     th.textContent = this.location;
     tr.appendChild(th);
-
+    
 
     var cookieSold = 0;
     var totalCookiesSold = 0;
@@ -95,19 +99,22 @@ Places.prototype.renderTable = function () {
     tr.appendChild(td);
 }
 function renderHeader() {
-    var parentElementTable = document.getElementById("TotalSold");
+    // var parentElementTable = document.getElementById("TotalSold");
 
     var article = document.createElement('article');
     parentElementTable.appendChild(article);
 
     var h2 = document.createElement('h2');
-    h2.textContent = 'Table of total sold';
+    h2.textContent = 'Table of total sold per day';
     article.appendChild(h2);
 
 }
 
+
+
+
 function renderHours() {
-    var parentElementTable = document.getElementById("TotalSold");
+    // var parentElementTable = document.getElementById("TotalSold");
 
     var article = document.createElement('article');
     parentElementTable.appendChild(article);
@@ -118,28 +125,45 @@ function renderHours() {
     var firstTr = document.createElement('tr')
     table.appendChild(firstTr);
 
-    var emptyHeader = document.createElement('th')
-    emptyHeader.textContent = ' ';
-    firstTr.appendChild(emptyHeader);
+    // var emptyHeader = document.createElement('th')
+    // emptyHeader.textContent = ' ';
+    // firstTr.appendChild(emptyHeader);
+
+    var space = document.createElement('th');
+    space.textContent='CITY    ';
+    firstTr.appendChild(space);
+
+    // var totalHour=0;
 
     for (var i = 0; i < workHours.length; i++) {
         var header = document.createElement('th');
         header.textContent = workHours[i];
         firstTr.appendChild(header);
     }
-    var article = document.createElement('article');
-    parentElementTable.appendChild(article);
+    // var article = document.createElement('article');
+    // parentElementTable.appendChild(article);
 
     var dailyTotal = document.createElement('th');
-    article.appendChild(dailyTotal);
-    dailyTotal.textContent - 'Daily Location Total';
+    dailyTotal.textContent = 'Daily Location Total';
     firstTr.appendChild(dailyTotal);
+
 }
+// =====================
+// =====================
+// =====================
+// =====================
+// trial code !!!! 
+// =====================
+// =====================
+// =====================
+// =====================
+
 renderHeader();
 renderHours();
 seattlLocation.renderTable();
-// tokyoLocation.renderTable();
-// dubaiLocation.renderTable();
-// parisLocation.renderTable();
-// limaLocation.renderTable();
+tokyoLocation.renderTable();
+dubaiLocation.renderTable();
+parisLocation.renderTable();
+limaLocation.renderTable();
+
 
